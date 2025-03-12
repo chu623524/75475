954630a5dd341590508f4ffcdf48e71ea139f111
chu623524 <2548663466@qq.com>
2025-03-12 22:08:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 from io import BytesIO
 
 # 设置模型和标准化器的路径
-MODEL_URL = 'https://raw.githubusercontent.com/chu623524/323457/main/RF.pkl'
-SCALER_URL = 'https://raw.githubusercontent.com/chu623524/323457/main/scaler.pkl'
+MODEL_URL = 'https://raw.githubusercontent.com/chu623524/75475/main/RF1.pkl'
+SCALER_URL = 'https://raw.githubusercontent.com/chu623524/75475/main/scaler.pkl'
 
 # 下载模型和标准化器
 @st.cache_resource
